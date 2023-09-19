d42ff8edec777d8c98f6599a822ada6e995d8b91
João Mauricio <111187472+joaomauriciodev@users.noreply.github.com>
2023-09-19 23:29:35 +0000

    Add scripts  and

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 tabnews.com.br implemetation.
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
 function Home() {
-    return(
-        <div>
-            <h1>New Commit</h1>
-            <p>Teste de Deploy</p>
-        </div>
-      
-    )
+  return (
+    <div>
+      <h1>New Commit</h1>
+      <p>Teste de Deploy</p>
+    </div>
+  );
 }
 
 export default Home;
